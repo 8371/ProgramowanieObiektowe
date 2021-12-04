80e251d1bf7187ad143531c0b7e51433d5266c8b
8371 <38053146+8371@users.noreply.github.com>
2021-12-04 02:58:32 +0100

    Create produkt.cs

---
+++ ZadaniaPO/produkt.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania_PO
+{
+    class produkt
+    {
+        public string nazwa;
+        public double cena;
+        public produkt(string nazwa, double cena)
+        {
+            this.nazwa = nazwa;
+            this.cena = cena;
+        }
+    }
+}
